b0a8b8e833c1233dd491c8f9978e6f5ba3e40744
Triana Padilla <triana@ournameismud.co.uk>
2020-06-10 21:51:53 +0100

    changed public path

---
+++ deploy/public_html/dist/js/3.bundle.1591822263052.js
@@ -0,0 +1,3 @@
+/*! For license information please see 3.bundle.1591822263052.js.LICENSE.txt */
+(this.webpackJsonp=this.webpackJsonp||[]).push([[3],{11:function(e,t,n){"use strict";n.r(t);var o=n(26),c=n.n(o);t.default=function({node:e}){const t=new c.a(e,{cellAlign:"left",contain:!1,pageDots:!1,wrapAround:!0,prevNextButtons:!1,cellSelector:".c-carousel__item"});return()=>{t.destroy()}}}}]);
+//# sourceMappingURL=3.bundle.1591822263052.js.map
--- deploy/public_html/dist/js/4.bundle.1591822263052.js
+++ deploy/public_html/dist/js/4.bundle.1591822263052.js
@@ -1,3 +1,3 @@
-/*! For license information please see 4.bundle.1591811382286.js.LICENSE.txt */
+/*! For license information please see 4.bundle.1591822263052.js.LICENSE.txt */
 (this.webpackJsonp=this.webpackJsonp||[]).push([[4],{14:function(t,e,n){"use strict";n.r(e);var s=n(0);e.default=Object(s.withPlugins)(s.domEvents)((function({node:t,name:e,plugins:{addEvents:n}}){return n({"click [data-link]":function(t,e){t.preventDefault(),e.classList.toggle("huzzah")}}),log(this,t,e),()=>{log("i am called when the module is destroyed")}}))}}]);
-//# sourceMappingURL=4.bundle.1591811382286.js.map
+//# sourceMappingURL=4.bundle.1591822263052.js.map
